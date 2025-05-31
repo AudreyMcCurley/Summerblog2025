b69d6b4bb9ac2ee05290c0d2c8e7328d439c576b
Audrey <student30.gs4t@gmail.com>
2025-05-31 14:49:07 -0700

    basicjavascript

---
+++ script.js
@@ -0,0 +1,12 @@
+let name = "Audrey";
+let age = 13;
+let likesSushi = true;
+
+function greeting(name) {
+console.log("Hello", name);
+}
+
+greeting("Audrey");
+
+let newName = document.getElementById("greeting").value;
+
